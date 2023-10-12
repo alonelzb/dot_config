0eadfc5f873ed9e51ceb6eba3ce5123098422b1c
alonelzb <41935263+alonelzb@users.noreply.github.com>
2023-10-12 08:57:08 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 ln -s $HOME/dot_config $HOME/.config
 ln -s $HOME/dot_config/.zshenv $HOME/.zshenv
 
-pip3 install requests tqdm sh
+pip3 install requests tqdm sh pynvim
 python3 $HOME/.config/scripts/install.py
+
+npm i -g neovim
 
 echo done
