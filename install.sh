0900d6fab2d976bf0a53db671d0cfaedfb13d849
luozaibo <zaiboluo@gmail.com>
2023-10-15 21:35:14 -0400

    rocky update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
-ln -s $HOME/dot_config $HOME/.config
-ln -s $HOME/dot_config/.zshenv $HOME/.zshenv
+if [[ ! -e $HOME/.config ]]; then
+	ln -s $HOME/dot_config $HOME/.config
+	ln -s $HOME/dot_config/.zshenv $HOME/.zshenv
+fi
 
 pip3 install requests tqdm sh pynvim
 python3 $HOME/.config/scripts/install.py
