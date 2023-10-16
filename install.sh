219e550221a24b631987405d1aaea2b935dc5cd2
alonelzb <41935263+alonelzb@users.noreply.github.com>
2023-10-16 09:42:38 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ if [[ ! -e $HOME/.config ]]; then
 	ln -s $HOME/dot_config/.zshenv $HOME/.zshenv
 fi
 
+curl -fsSL https://fnm.vercel.app/install | sh
+
 pip3 install requests tqdm sh pynvim
 python3 $HOME/.config/scripts/install.py
 
